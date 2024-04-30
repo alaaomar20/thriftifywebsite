30164d594f7fc1636231f262371bd8994f34af10
vaagz <alshagawi.shams@gmail.com>
2024-04-30 04:59:05 +0300

    feat: Add filter functionality to homepage; make detailed product page dynamic

--- user/DetailedPage.php
+++ user/DetailedPage.php
@@ -4,10 +4,10 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta name="description" content="Discover a variety of pre-loved Women's clothing at Thriftify. Shop sustainable fashion with our wide range of dresses, jackets, pants, and more." />
-  <meta name="keywords" content="Women's clothing, thrift shop, sustainable fashion, pre-loved clothes, second-hand dresses, affordable fashion, eco-friendly clothing, thrift store" />
-  <title>Thrift Shop - Women's Clothing</title>
-  <link rel="stylesheet" href="MenWomen.css" />
+  <meta name="keywords" content="MKI Hoodie, Phonetic Hoodie, Men's Fashion, Casual Wear, High-Quality Cotton, Sustainable Fashion, Eco-Friendly Clothing, Thrift Store" />
+  <meta name="description" content="Explore the MKI Phonetic Hoodie on THRIFTIFY. This high-quality cotton hoodie combines style and comfort, perfect for your casual wardrobe." />
+  <title>Product Detail Page</title>
+  <link rel="stylesheet" href="Detail.css" />
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Roboto&display=swap" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
 </head>
@@ -48,28 +48,24 @@
                   <path d="M0.877197 7.49984C0.877197 3.84216 3.84234 0.877014 7.50003 0.877014C11.1577 0.877014 14.1229 3.84216 14.1229 7.49984C14.1229 11.1575 11.1577 14.1227 7.50003 14.1227C3.84234 14.1227 0.877197 11.1575 0.877197 7.49984ZM7.50003 1.82701C4.36702 1.82701 1.8272 4.36683 1.8272 7.49984C1.8272 10.6328 4.36702 13.1727 7.50003 13.1727C10.633 13.1727 13.1729 10.6328 13.1729 7.49984C13.1729 4.36683 10.633 1.82701 7.50003 1.82701ZM7.12457 9.00001C7.06994 9.12735 6.33165 11.9592 6.33165 11.9592C6.26018 12.226 5.98601 12.3843 5.71928 12.3128C5.45255 12.2413 5.29425 11.9672 5.36573 11.7004C5.36573 11.7004 6.24661 8.87268 6.24661 8.27007V6.80099L4.28763 6.27608C4.0209 6.20461 3.86261 5.93045 3.93408 5.66371C4.00555 5.39698 4.27972 5.23869 4.54645 5.31016C4.54645 5.31016 6.20042 5.87268 6.84579 5.87268H8.15505C8.80042 5.87268 10.4534 5.31042 10.4534 5.31042C10.7202 5.23895 10.9943 5.39724 11.0658 5.66397C11.1373 5.93071 10.979 6.20487 10.7122 6.27635L8.74661 6.80303V8.27007C8.74661 8.87268 9.62663 11.6971 9.62663 11.6971C9.6981 11.9639 9.5398 12.238 9.27307 12.3095C9.00634 12.381 8.73217 12.2227 8.6607 11.956C8.6607 11.956 7.91994 9.12735 7.86866 9.00001C7.81994 8.87268 7.65006 8.87268 7.65006 8.87268H7.34317C7.34317 8.87268 7.16994 8.87268 7.12457 9.00001ZM7.50043 5.12007C8.12175 5.12007 8.62543 4.61639 8.62543 3.99507C8.62543 3.37375 8.12175 2.87007 7.50043 2.87007C6.87911 2.87007 6.37543 3.37375 6.37543 3.99507C6.37543 4.61639 6.87911 5.12007 7.50043 5.12007Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
                 </svg></i>About</a>
           </li>
-          <li class="nav-item">
-            <a href="contactus.html" class="nav-link"><i class="bi bi-bag" style="margin-right: 20px"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M1 2C0.447715 2 0 2.44772 0 3V12C0 12.5523 0.447715 13 1 13H14C14.5523 13 15 12.5523 15 12V3C15 2.44772 14.5523 2 14 2H1ZM1 3L14 3V3.92494C13.9174 3.92486 13.8338 3.94751 13.7589 3.99505L7.5 7.96703L1.24112 3.99505C1.16621 3.94751 1.0826 3.92486 1 3.92494V3ZM1 4.90797V12H14V4.90797L7.74112 8.87995C7.59394 8.97335 7.40606 8.97335 7.25888 8.87995L1 4.90797Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
-                </svg></i>contact us
-            </a>
-          </li>
         </ul>
       </div>
     </div>
   </nav>
 
+  <!-- Main Content -->
   <?php
   require_once "db_connection.php";
 
-  // Initialize selected category variable
-  $selectedCategory = $_GET['category'] ?? 'women'; // Default to 'women' if category is not set
-
-  // Query to select products based on the selected category
-  $query = "SELECT * FROM products WHERE Product_Category = '$selectedCategory'";
+  // Check if the ID parameter exists in the URL
+  if (isset($_GET['id'])) {
+    // Sanitize the ID parameter to prevent SQL injection
+    $product_id = htmlspecialchars($_GET['id']);
+  }
+  $query = "SELECT * FROM products WHERE Product_ID = '$product_id'";
   $result = mysqli_query($connection, $query);
 
-  // Initialize an array to store product information
+
   $products = array();
 
   // Check if there are any products in the database
@@ -82,6 +78,8 @@
       $productDescription = $row['Product_Description'];
       $productPrice = $row['Product_Price'];
       $productImage = $row['Product_Img_URL'];
+      $productQuantity = $row['Product_Quantity'];
+
 
       // Store product information in an associative array
       $product = array(
@@ -89,7 +87,8 @@
         'name' => $productName,
         'description' => $productDescription,
         'price' => $productPrice,
-        'image' => $productImage
+        'image' => $productImage,
+        'quantity' => $productQuantity
       );
 
       // Push the product array into the products array
@@ -97,109 +96,155 @@
     }
   }
 
-
-  $queryMaxID = "SELECT COUNT(*) AS max_id FROM products WHERE Product_Category = '$selectedCategory'";
-  $resultMaxID = mysqli_query($connection, $queryMaxID);
-
-  // Initialize a variable to store the maximum ID
-  $maxID = 0;
-
-  // Check if the query was successful
-  if ($resultMaxID) {
-    // Fetch the result as an associative array
-    $rowMaxID = mysqli_fetch_assoc($resultMaxID);
-
-    // Get the maximum ID
-    $maxID = $rowMaxID['max_id'];
-  } else {
-    // Display an error message if the query fails
-    echo "Error: " . mysqli_error($connection);
-  }
-
-
-
-  // Close the database connection (optional, as PHP will automatically close it when the script ends)
-  mysqli_close($connection);
   ?>
-  <!-- Main Content -->
-  <!-- Buttons Section -->
 
+  <!-- Buttons Section -->
   <!-- Buttons with Dropdown Menus -->
-  <div class="d-flex justify-content-between">
-    <div style="margin-top: 60px">
-      <select id="categoryDropdown" class="form-select m-3">
-        <option value="women">Women</option>
-        <option value="men">Men</option>
-      </select>
+
+
+  <div class="product-detail-container" style="margin-top: 100px;">
+    <div class="product-image-container">
+      <img src="<?php echo $product['image']; ?>" alt="<?php echo $product['name']; ?>" class="product-image" />
     </div>
 
+    <div class="product-info-container">
+      <h1 class="product-name"><?php echo $product['name']; ?></h1>
+      <p class="product-description"><?php echo $product['description']; ?></p>
+      <p class="product-price">$<?php echo $product['price']; ?></p>
+      <p class="size-guide" onclick="openSizeGuide()">Size Guide 𖣳</p>
+      <div class="size-selection">
+        <table>
+          <tr>
+            <td>Small</td>
+            <td>Medium</td>
+          </tr>
+        </table>
+      </div>
+      <p class="quantity-label">In stock: <?php echo $product['quantity']; ?></p>
 
+      <!-- Quantity Selector -->
+      <div class="quantity-selector">
+        <button onclick="decrementQuantity()" aria-label="Decrease quantity">
+          -
+        </button>
+        <input type="number" id="quantity" value="1" min="1" readonly />
+        <button onclick="incrementQuantity()" aria-label="Increase quantity">
+          +
+        </button>
+      </div>
+
+      <div class="add-to-cart-container">
+        <a href="./shoppingcart.html" class="add-to-cart-link">
+          <button class="add-to-cart-button" type="button" style="width: 120px; height: 40px; margin: 15px">
+            Add to Cart
+          </button>
+        </a>
+      </div>
+
+      <div class="product-description">
+        <details>
+          <summary>Detailed Description</summary>
+          <p>
+            Speak the language of style with <?php echo $product['name']; ?>. Made
+            from soft, high-quality fabric, it's adorned with a phonetic print
+            and is sure to be a cool and casual addition to any wardrobe.
+          </p>
+          <ul>
+            <li>100% Cotton</li>
+            <li>Drawstring Hood</li>
+            <li>Kangaroo Pocket</li>
+            <li>Ribbed Trims</li>
+            <li>Printed Branding</li>
+          </ul>
+        </details>
+      </div>
+    </div>
   </div>
 
-
-  <main>
-    <div class="item-count">
-      <p>Items Available: <?php echo $maxID; ?> </p>
-      <!-- Adjust the number of items as needed -->
+  <div id="size-guide-modal" class="modal">
+    <div class="modal-content">
+      <span class="close" onclick="closeSizeGuide()">&times;</span>
+      <img src="../images/size.jpg" alt="Size Guide" />
     </div>
-    <!-- Row grid -->
+  </div>
 
-    <div class="product-grid">
-      <?php foreach ($products as $product) : ?>
-        <div class="product-card">
-          <a href="DetailedPage.php?id=<?php echo $product['id']; ?>">
-            <img src="<?php echo $product['image']; ?>" alt="<?php echo $product['name']; ?>" class="product-image" />
-            <div class="product-info">
-              <p class="product-name"><?php echo $product['name']; ?></p>
-              <p class="product-description"><?php echo $product['description']; ?></p>
-              <p class="product-price">$<?php echo $product['price']; ?></p>
-            </div>
-          </a>
-        </div>
-      <?php endforeach; ?>
-    </div>
+  <script>
+    // Dummy functions for placeholder actions
+    function openSizeGuide() {
+      document.getElementById("size-guide-modal").style.display = "block";
+    }
 
+    function closeSizeGuide() {
+      document.getElementById("size-guide-modal").style.display = "none";
+    }
 
-    <script>
-      let selectmenu = document.querySelector('#categoryDropdown');
-      selectmenu.addEventListener('click', function(e) {
-        let selectedCategory = e.target.value;
-        window.location.href = 'Women.php?category=' + selectedCategory;
-      });
-    </script>
+    function incrementQuantity() {
+      var quantity = parseInt(document.getElementById("quantity").value, 10);
+      document.getElementById("quantity").value = quantity + 1;
+    }
 
+    function decrementQuantity() {
+      var quantity = parseInt(document.getElementById("quantity").value, 10);
+      if (quantity > 1) {
+        document.getElementById("quantity").value = quantity - 1;
+      }
+    }
+  </script>
+</body>
 
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+<script>
+  // Get the quantity value
+  var quantity = parseInt(<?php echo $product['quantity']; ?>);
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-    <!-- Footer -->
-    <footer class="bg-dark text-light py-3 text-center" style="margin-top: -159">
+  // Function to update button state and label based on quantity
+  function updateButtonState() {
+    var addButton = document.querySelector('.add-to-cart-button');
+    var quantityInput = document.getElementById('quantity');
+    var quantityLabel = document.querySelector('.quantity-label');
 
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <a class="nav-link text-light" href="contactus.html">Contact Us</a>
-          </div>
-          <div class="col">
-            <a class="nav-link text-light" href="#">FAQs</a>
-          </div>
-          <div class="col">
-            <a class="nav-link text-light" href="#">Country/Region: Saudi Arabia</a>
-          </div>
-        </div>
-        <div class="row mt-2">
-          <!-- Added margin top to create space -->
-          <div class="col">
-            <p>
-              THRIFTIFY and the THRIFTIFY logo are trademarks of Thriftify and
-              are registered or pending registration in numerous jurisdictions
-              around the world. &copy; Copyright 2024 Thriftify. All rights
-              reserved.
-            </p>
-          </div>
-        </div>
+    if (quantity === 0) {
+      addButton.disabled = true;
+      quantityInput.disabled = true;
+      quantityLabel.textContent = 'Not Available';
+    } else {
+      addButton.disabled = false;
+      quantityInput.disabled = false;
+      quantityLabel.textContent = 'In stock: ' + quantity;
+    }
+  }
+
+  // Call the function to initially set button state
+  updateButtonState();
+</script>
+
+<!-- Footer -->
+<footer class="bg-dark text-light py-3 text-center">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <a class="nav-link text-light" href="contactus.html">Contact Us</a>
       </div>
-    </footer>
+      <div class="col">
+        <a class="nav-link text-light" href="#">FAQs</a>
+      </div>
+      <div class="col">
+        <a class="nav-link text-light" href="#">Country/Region: Saudi Arabia</a>
+      </div>
+    </div>
+    <div class="row mt-2">
+      <!-- Added margin top to create space -->
+      <div class="col">
+        <p>
+          THRIFTIFY and the THRIFTIFY logo are trademarks of Thriftify and are
+          registered or pending registration in numerous jurisdictions around
+          the world. &copy; Copyright 2024 Thriftify. All rights reserved.
+        </p>
+      </div>
+    </div>
+  </div>
+</footer>
 
 </html>
